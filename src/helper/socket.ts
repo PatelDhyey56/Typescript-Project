30f03157be018ba9d36f247af948960aa243fbf4
PatelDhyey56 <patekdhyey200@gmail.com>
2024-10-02 18:07:33 +0530

    Group socket added

--- src/helper/socket.ts
+++ src/helper/socket.ts
@@ -2,16 +2,6 @@ import { instrument } from "@socket.io/admin-ui";
 import { Server } from "socket.io";
 import { httpServer } from "..";
 
-let SocketInfo = {
-  brodCastLimit: 10000,
-};
-
-let peoplesTalk: {
-  id: number;
-  name: string;
-  messages: string;
-}[] = [];
-
 const io = new Server(httpServer, {
   cors: {
     origin: ["https://admin.socket.io"],
@@ -24,28 +14,93 @@ instrument(io, {
   mode: "development",
 });
 
-let chat = io.of("chat");
+let SocketInfo = {
+  brodCastLimit: 10000,
+};
+
+let peoplesTalk: {
+  id: number;
+  name: string;
+  messages: string;
+}[] = [];
+
+let GroupInfo: {
+  id: number;
+  name: string;
+  messages: { msg?: string; time?: Date }[];
+  createdAt: Date;
+}[] = [];
+
+let GroupMessages: { messages?: string; time?: Date }[] = [];
+
+const chat = io.of("chat");
+const group = io.of("group");
+
 chat.on("connection", (socket) => {
-  console.log("User connected with id:", socket.id);
-  socket.join("Room");
+  console.log("User connected to Talk with id:", socket.id);
+
+  socket.join(`Room`);
   socket.on("msg1", (name: string, msg: string) => {
     peoplesTalk.push({ id: 1, name, messages: msg });
-    socket.broadcast.emit("msgList", peoplesTalk);
-    io.emit("msgList", peoplesTalk);
+    io.of("/chat").emit("msgList", peoplesTalk);
   });
 
   socket.on("msg2", (name: string, msg: string) => {
     peoplesTalk.push({ id: 2, name, messages: msg });
-    io.emit("msgList", peoplesTalk);
+    io.of("/chat").emit("msgList", peoplesTalk);
   });
 
-  // socket.broadcast.emit("msgList", peoplesTalk);
+  socket.on("typing", function (id: string) {
+    socket.broadcast.emit(`User ${peoplesTalk[+id - 1].name} is typing....`);
+  });
+
+  socket.on("disconnect", () =>
+    console.log("User Dis-Connected with id:", socket.id)
+  );
+});
+
+group.on("connection", (socket) => {
+  console.log("User Join in Group with id:", socket.id);
+
+  socket.on("AddMember", (name: string) => {
+    socket.join(`Group`);
+    GroupInfo.push({
+      id: GroupInfo.length,
+      name,
+      messages: [],
+      createdAt: new Date(),
+    });
+    GroupMessages.unshift({ messages: `${name} Added...`, time: new Date() });
+    io.of("/group").emit("Group-newMessage", "Member Added...", GroupInfo);
+  });
+
+  socket.on("GroupMessage", (id: number, msg: string) => {
+    id = +id - 1;
+    try {
+      GroupInfo[id].messages.push({ msg, time: new Date() });
+      GroupMessages.unshift({
+        messages: msg,
+        time: new Date(),
+      });
+      io.of("/group").emit("Group-newMessage", GroupMessages.reverse());
+    } catch (e) {
+      socket.disconnect();
+    }
+  });
+
+  socket.on("typing", function (id: string) {
+    socket.broadcast.emit(
+      "typing",
+      `User ${GroupInfo[+id - 1].name} is typing....`
+    );
+  });
+
   socket.on("disconnect", () =>
     console.log("User Dis-Connected with id:", socket.id)
   );
 });
 
 setTimeout(
-  () => io.to("Room").emit("Room", "Hello All Members:)"),
+  () => io.of("/chat").to("Room").emit("Room", "Hello All Members:)"),
   SocketInfo.brodCastLimit
 );
